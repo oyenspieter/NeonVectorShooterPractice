c4b01c9ce2d91605f8061d99c010461f329a5aa7
5720508 <p.m.w.marezoyens@student.uu.nl>
2015-12-08 16:34:10 +0100

    Added back-end Entity abstract class and entity manager class

--- NeonVectorShooter/NeonVectorShooter/Enitity.cs
+++ NeonVectorShooter/NeonVectorShooter/Enitity.cs
@@ -15,9 +15,22 @@ namespace NeonVectorShooter
 
         public Vector2 position, velocity;
         public float orientation;
-        public float radius;
-        public bool isExpired;
+        public float radius = 20; // used for circular collision detection
+        public bool isExpired; // true if the entity is destroyed and should be deleted 
 
-        
+        public Vector2 Size
+        {
+            get
+            {
+                return image == null ? Vector2.Zero : new Vector2(image.Width, image.Height); 
+            }
+        }
+
+        public abstract void Update();
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, position, null, color, orientation, Size / 2f, 1f, 0, 0);
+        }
     }
 }
---
+++ NeonVectorShooter/NeonVectorShooter/EnitityManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeonVectorShooter
+{
+    static class EnitityManager
+    {
+        static List<Enitity> entities = new List<Enitity>();
+
+        static bool isUpdating;
+        static List<Enitity> addedEntities = new List<Enitity>();
+
+        public static int Count
+        {
+            get { return entities.Count; }
+        }
+
+        public static void Add(Enitity entity)
+        {
+            if (!isUpdating)
+                entities.Add(entity);
+            else
+                addedEntities.Add(entity);
+        }
+
+        public static void Update()
+        {
+            isUpdating = true;
+
+            foreach (var entity in entities)
+                entity.Update();
+
+            isUpdating = false;
+
+            foreach (var entity in addedEntities)
+                entities.Add(entity);
+
+            addedEntities.Clear();
+
+            entities = entities.Where(x => !x.isExpired).ToList();
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var entity in entities)
+                entity.Draw(spriteBatch);
+        }
+    }
+}
