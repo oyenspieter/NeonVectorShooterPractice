ec88c915c37adc8bc6bbb838a8d8bae61f301a2c
5720508 <p.m.w.marezoyens@student.uu.nl>
2015-12-08 15:57:50 +0100

    First Commit, Hello World!

---
+++ NeonVectorShooter/NeonVectorShooter/Enitity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeonVectorShooter
+{
+    abstract class Enitity
+    {
+        protected Texture2D image;
+        // The tint of the image. This allows us to chagne the transparancy
+        protected Color color = Color.White;
+
+        public Vector2 position, velocity;
+        public float orientation;
+        public float radius;
+        public bool isExpired;
+
+        
+    }
+}
